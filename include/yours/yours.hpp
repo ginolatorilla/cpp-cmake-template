82ec3a619291d252305e41b61d7be1df3267207b
Gino Latorilla <pblatorilla+github@gmail.com>
2019-03-18 02:14:14 +0800

    Added initial library files

---
+++ include/yours/yours.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 Gino Latorilla.
+
+#ifndef YOURS_YOURS_HPP_
+#define YOURS_YOURS_HPP_
+
+namespace yours
+{
+
+int do_something();
+
+} // namespace yours
+
+#endif  // YOURS_YOURS_HPP_
---
+++ src/impl.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 Gino Latorilla.
+
+namespace yours
+{
+
+int do_something()
+{
+    return 0;
+}
+
+} // namespace yours
+
