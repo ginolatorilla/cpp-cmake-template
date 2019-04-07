8c897a86a45d73667a4f3f2b1b040d260d23fd66
Gino Latorilla <pblatorilla+github@gmail.com>
2019-04-07 18:26:15 +0800

    Extracted build targets

---
+++ cmake/_section_targets.cmake
@@ -0,0 +1,33 @@
+# Copyright (c) 2019 Gino Latorilla.
+#
+# Declare all build targets here.
+
+# Guarantees that this file be included once.
+# You can't declare build targets more than once.
+if(__section_targets)
+    return()
+endif()
+set(__section_targets INCLUDED)
+
+set(${PROJECT_NAME}_SOURCES src/impl.cpp)
+
+if(${PROJECT_NAME}_BUILD_SHARED)
+    set(_library_target_type SHARED)
+else()
+    set(_library_target_type STATIC)
+endif()
+
+# This declares a library target (could be shared or static).
+# Expose your API directories with the INTERFACE property.
+add_library(${PROJECT_NAME} ${_library_target_type} ${${PROJECT_NAME}_SOURCES})
+target_include_directories(${PROJECT_NAME}
+    INTERFACE
+        include
+)
+
+# This declares an executable target.
+# But sometimes we want `libXYZ` and `XYZ.exe`, so we want the OUTPUT_NAME
+# to force that without clashing with CMake build target names.
+add_executable(${PROJECT_NAME}_exe src/main.cpp)
+set_target_properties(${PROJECT_NAME}_exe PROPERTIES OUTPUT_NAME ${PROJECT_NAME})
+
