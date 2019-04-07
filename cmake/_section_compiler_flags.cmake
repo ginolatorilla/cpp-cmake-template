02d7b13b11d3c93b2f0f3eb73a8a917ce60b40f2
Gino Latorilla <pblatorilla+github@gmail.com>
2019-04-07 18:58:24 +0800

    Added compilation flags

---
+++ cmake/_section_compiler_flags.cmake
@@ -0,0 +1,49 @@
+# Copyright (c) 2019 Gino Latorilla.
+#
+# Declare global compilation flags here.
+# I encourage you to use `target_compile_definitions` (for -D<FLAGS>)
+# and `target_compiler_options` for target-specific settings.
+
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+macro(add_cxx_flag _flag)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_flag}")
+endmacro()
+
+# General compiler feature flags
+add_cxx_flag(-fPIC)
+add_cxx_flag(-fdiagnostics-color=always)
+add_cxx_flag(-pipe)
+
+# I prefer to use exceptions only during tests.
+if(${PROJECT_NAME}_ENABLE_TESTING)
+    add_cxx_flag(-fexceptions)
+else()
+    add_cxx_flag(-fno-exceptions)
+endif()
+
+# I prefer strict compliance to standards.
+add_cxx_flag(-Wall)
+add_cxx_flag(-Werror)
+add_cxx_flag(-Wextra)
+add_cxx_flag(-pedantic)
+
+macro(add_cxx_flag_for_build _build_type _flag)
+    set(CMAKE_CXX_FLAGS_${_build_type} "${CMAKE_CXX_FLAGS} ${_flag}")
+endmacro()
+
+add_cxx_flag_for_build(DEBUG -Og)
+add_cxx_flag_for_build(DEBUG -ggdb3)
+add_cxx_flag_for_build(MINSIZEREL -Os)
+add_cxx_flag_for_build(RELEASE -O3)
+add_cxx_flag_for_build(RELWITHDEBINFO -Og)
+
+macro(add_exe_linker_flag _flag)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${_flag}")
+endmacro()
+
+add_exe_linker_flag(-fuse-ld=gold)
+add_exe_linker_flag(-Wl,--disable-new-dtags)
+add_exe_linker_flag(-Wl,--no-as-needed)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright (c) 2019 Gino Latorilla.
 
+#define UNUSED_VAR(__X__) do{ static_cast<void>(__X__); } while(0);
+
 int main(int argc, char* argv[])
 {
+    UNUSED_VAR(argc);
+    UNUSED_VAR(argv);
     return 0;
 }
+
+#undef UNUSED_VAR
